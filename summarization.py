76f4c244a72413921f4f5335c2e8e3ebc9fbead0
SWARAJ <101943432+SWARAJ-KADU@users.noreply.github.com>
2024-07-03 13:54:27 +0530

    Update summarization.py

--- summarization.py
+++ summarization.py
@@ -1,10 +1,13 @@
 from transformers import BartTokenizer, BartForConditionalGeneration
+import torch
 
 model_name = "facebook/bart-large-cnn"
 tokenizer = BartTokenizer.from_pretrained(model_name)
 model = BartForConditionalGeneration.from_pretrained(model_name)
 
-# Define the summarization function
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
 def summarize_text(text):
     inputs = tokenizer(text, return_tensors="pt", max_length=1024, truncation=True)
     summary_ids = model.generate(inputs.input_ids, max_length=2500, min_length=500, length_penalty=2.0, num_beams=4, early_stopping=True)
