79c97f73d1798983d8a8f697732fb2c1dacd7902
SWARAJ <101943432+SWARAJ-KADU@users.noreply.github.com>
2024-07-03 13:54:56 +0530

    Update summarization.py

--- summarization.py
+++ summarization.py
@@ -9,7 +9,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
 def summarize_text(text):
-    inputs = tokenizer(text, return_tensors="pt", max_length=1024, truncation=True)
+    inputs = tokenizer(text, return_tensors="pt", max_length=1024, truncation=True).to(device)
     summary_ids = model.generate(inputs.input_ids, max_length=2500, min_length=500, length_penalty=2.0, num_beams=4, early_stopping=True)
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return summary
